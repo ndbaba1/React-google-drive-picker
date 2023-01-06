ee5c8f78cdfab5c5e0216f5bec2fa107e67f369e
nnamdi ogundu <nnamdi.ogundu@outsourceglobal.com>
2023-01-06 13:20:26 +0100

    added parent folder for view mode

--- src/index.tsx
+++ src/index.tsx
@@ -125,6 +125,7 @@ export default function useDrivePicker(): [
     if (viewMimeTypes) view.setMimeTypes(viewMimeTypes)
     if (setIncludeFolders) view.setSelectFolderEnabled(true)
     if (setSelectFolderEnabled) view.setSelectFolderEnabled(true)
+    if (setParentFolder) view.setParent(setParentFolder)
 
     const uploadView = new google.picker.DocsUploadView()
     if (viewMimeTypes) uploadView.setMimeTypes(viewMimeTypes)
